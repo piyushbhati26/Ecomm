17167e5e8e0e6ab91f00c94ff43cf5a167b7791c
Piyush Bhati <bhatip26@gmail.com>
2022-11-19 22:54:34 +0530

    19/11

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -2,15 +2,32 @@
 let express = require("express");
 let categoryRouter = express.Router();
 let categoryController = require("./../controller/category.controller");
+let requestValidator = require("./../middlewares/RequestValidator");
 
 categoryRouter.get("/", categoryController.getAllCategories);
 
-categoryRouter.get("/:categoryId", categoryController.getCategoryById);
+categoryRouter.get(
+  "/:categoryId",
+  [requestValidator.validateReqForCategoryId],
+  categoryController.getCategoryById
+);
 
-categoryRouter.post("/", categoryController.addNewCategory);
+categoryRouter.post(
+  "/",
+  [requestValidator.validateReqForCategoryName],
+  categoryController.addNewCategory
+);
 
-categoryRouter.delete("/:categoryId", categoryController.deleteCategoryById);
+categoryRouter.delete(
+  "/:categoryId",
+  [requestValidator.validateReqForCategoryName],
+  categoryController.deleteCategoryById
+);
 
-categoryRouter.put("/:categoryId", categoryController.updateCategoryById);
+categoryRouter.put(
+  "/:categoryId",
+  [requestValidator.validateReqForCategoryId],
+  categoryController.updateCategoryById
+);
 
 module.exports = categoryRouter;
